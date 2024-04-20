add9f6ff5acc06435b0e479e9c6cf0d7f03e4a1b
dioni5 <151408301+dioni5@users.noreply.github.com>
2024-04-20 03:04:00 +0200

    krijimi i tabeles

---
+++ Polybius-Square-Cipher.py
@@ -0,0 +1,7 @@
+def generate_polybius_square():
+    grid = [['A', 'B', 'C', 'D', 'E'],
+            ['F', 'G', 'H', 'I', 'K'],
+            ['L', 'M', 'N', 'O', 'P'],
+            ['Q', 'R', 'S', 'T', 'U'],
+            ['V', 'W', 'X', 'Y', 'Z']]
+    return grid
