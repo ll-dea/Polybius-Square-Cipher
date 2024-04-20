498db3d2eeaffa9d3699ba39014479ebeff517bd
Diongashi09 <dion.gashi2@student.uni-pr.edu>
2024-04-20 19:06:18 +0200

    update

--- Polybius-Square-Cipher.py
+++ Polybius-Square-Cipher.py
@@ -5,3 +5,13 @@ def generate_polybius_square():
             ['Q', 'R', 'S', 'T', 'U'],
             ['V', 'W', 'X', 'Y', 'Z']]
     return grid
+def encode_message(message, grid):
+    encoded_message = ""
+    for char in message:
+        if char.upper() == 'J':
+            char = 'I'
+        for i in range(5):
+            for j in range(5):
+                if grid[i][j] == char.upper():
+                    encoded_message += str(i+1) + str(j+1)
+    return encoded_message
